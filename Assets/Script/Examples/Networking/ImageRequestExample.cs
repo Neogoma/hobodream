27673f3d1a5b04ccb114768ec5d3ec7588b001ab
Heri Andrianjafy <heri@neogoma.com>
2022-01-27 09:59:36 +0800

    Update (#14)

--- Assets/Script/Examples/Networking/ImageRequestExample.cs
+++ Assets/Script/Examples/Networking/ImageRequestExample.cs
@@ -29,7 +29,7 @@ namespace Neogoma.Hobodream.Examples.Networking
 
         }
 
-        public void RequestFailed(long code,string jsonResult, string key)
+        public void RequestFailed(long code,string data,string jsonResult, string key)
         {
             
         }
